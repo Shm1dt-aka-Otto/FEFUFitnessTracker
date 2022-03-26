7695d87cffa1adf5a7005e411e25d42b9e37d105
Shm1dt-aka-Otto <andrey.siben@gmail.com>
2022-03-27 03:51:43 +1000

    Homework #1

---
+++ app/src/main/java/com/example/fefufitnesstracker/ui/TrackActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fefufitnesstracker.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.fefufitnesstracker.R
+
+class TrackFragment  : Fragment(R.layout.fragment_activity) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
